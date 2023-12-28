a486a733711afa2f9c41df42f2aa407d1ff18d58
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2023-12-28 18:51:45 +0700

    .

--- api/index.js
+++ api/index.js
@@ -6,10 +6,7 @@ const cors = require("cors");
 
 dotenv.config();
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB...");
   })
